01aa70ab4ffa4c9b892e48b4ab246c18379079f0
Starinin Andrey <starininandrey@gmail.com>
2022-08-18 19:43:16 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form>
+      <div>
+        <label for="first-name">First name</label>
+        <input type="text" id="first-name" v-model="first_name">
+      </div>
+      <div>
+        <label for="last-name">Last name</label>
+        <input type="text" id="last-name" v-model="last_name">
+      </div>
+      <div>
+        <label for="date-of-birth">Date of birth</label>
+        <input type="text" id="date-of-birth" v-model="date_of_birth">
+      </div>
+    </form>
+
+    <p v-if="last_name && first_name && date_of_birth">
+      {{last_name}} {{first_name}}, {{date_of_birth}}
+    </p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      first_name: '',
+      last_name: '',
+      date_of_birth: ''
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+form {
+  font-family: Arial, sans-serif;
+  font-size: 20pt;
+  padding: 10px;
+  margin: auto 30px;
+}
+input{
+  font-family: Arial, sans-serif;
+  font-size: 20pt;
+}
+div {
+  margin: 10px;
+  padding: 5px;
+}
+
+label {
+  margin: 3px;
+}
+
+p {
+  font-family: Arial, sans-serif;
+  font-size: 20pt;
+  color: darkblue;
 }
 </style>
